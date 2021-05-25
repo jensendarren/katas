af0bca937d8d8176e81323a1a4d91d3300f60966
Darren Jensen <darren.jensen@gmail.com>
2021-05-25 08:54:09 +0700

    added min heap kata

--- README.md
+++ README.md
@@ -11,3 +11,7 @@
 ### Dynamic Programming
 
 [Number of Ways to Make Change](./dynamic-programming/number-of-ways-to-make-change)
+
+### Heaps
+
+[Min Heap Construction](./heaps/min-heap-construction)
---
+++ heaps/min-heap-construction/solution.js
@@ -0,0 +1,66 @@
+class MinHeap {
+	constructor(array) {
+		this.heap = this.buildHeap(array);
+	}
+
+	// O(n) time / O(1) space
+	buildHeap(array) {
+		const firstParentIdx = Math.floor((array.length - 2) / 2)
+		for(let currentIdx = firstParentIdx; currentIdx >=0; currentIdx--) {
+			this.shiftDown(currentIdx, array.length -1, array);
+		}
+		return array;
+	}
+
+	shiftDown(currentIdx, endIdx, heap) {
+		let childOneIdx = currentIdx * 2 + 1;
+		while(childOneIdx <= endIdx) {
+			const childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
+			let idxToSwap;
+			if (childTwoIdx !==-1 && heap[childTwoIdx] < heap[childOneIdx]) {
+				idxToSwap = childTwoIdx
+			} else {
+				idxToSwap = childOneIdx
+			}
+			if (heap[idxToSwap] < heap[currentIdx]) {
+				this.swap(currentIdx, idxToSwap, heap)
+				currentIdx = idxToSwap
+				childOneIdx = currentIdx * 2 + 1;
+			} else {
+				return
+			}
+
+		}
+	}
+
+	shiftUp(currentIdx, heap) {
+		let parentIdx = Math.floor((currentIdx - 1) / 2)
+		while(currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
+			this.swap(currentIdx, parentIdx, heap)
+			currentIdx = parentIdx
+			parentIdx = Math.floor((currentIdx -1) / 2)
+		}
+	}
+
+	peek() {
+		return this.heap[0]
+	}
+
+	remove() {
+		this.swap(0, this.heap.length - 1, this.heap)
+		const valueToRemove = this.heap.pop()
+		this.shiftDown(0, this.heap.lenth -1, this.heap)
+		return valueToRemove
+	}
+
+	insert(value) {
+		this.heap.push(value)
+		this.shiftUp(this.heap.lenghth -1, this.heap)
+	}
+
+	swap(i, j, heap) {
+		const temp = heap[j]
+		heap[j] = heap[i]
+		heap[i] = temp
+	}
+}

fd63484307208dcec81655f7d972b8718b13c829
Darren Jensen <darren.jensen@gmail.com>
2021-07-09 10:36:10 +0700

    added notes to solution for phone number mn kata

--- recursion/phone-number-mnemonics/solution.js
+++ recursion/phone-number-mnemonics/solution.js
@@ -1,21 +1,35 @@
-
+// O(4^n * n) time | O(4^b * n) space
+// This is becasue we reach our base case 4^n times based on the max
+// number of possible letters fro a given digit (which is 4 letters when
+// the current digit is a 7 or a 9).
 function phoneNumberMnemonics(phoneNumber) {
+    // create empty mnemoic for our number containing zeros
     const currentMnemonic = new Array(phoneNumber.length).fill('0');
+    // create empty array that will contain one mnemoic
     const mnemonicFound = [];
 
+    // call the recursive function starting at index 0
     phoneNumberMnemonicsHelper(0, phoneNumber, currentMnemonic, mnemonicFound);
     return mnemonicFound;
 }
 
 function phoneNumberMnemonicsHelper(idx, phoneNumber, currentMnemonic, mnemonicFound) {
+    // base case: we are already at the last digit in the phone number
+    //so just add the currentMnemocic to the list of mnemoics found
     if(idx === phoneNumber.length) {
         const mnemonic = currentMnemonic.join('');
         mnemonicFound.push(mnemonic);
     } else {
+        // otherwise we are still building the current mnomic so we grab the current digit
+        // of the phone nunber at the current index
         const digit = phoneNumber[idx];
+        // grab the letters that represent that digit
         const letters = DIGIT_LETTERS[digit];
+        // loop all of the lettes one by one
         for (const letter of letters) {
+            // set the current letter at the current index
             currentMnemonic[idx] = letter;
+            // call the function recursivly simply increaseing the index by 1
             phoneNumberMnemonicsHelper(idx + 1, phoneNumber, currentMnemonic, mnemonicFound);
         }
     }
